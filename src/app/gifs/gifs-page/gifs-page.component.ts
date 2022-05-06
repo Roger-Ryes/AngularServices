867f8aad44aa167a78a42b52955412811c68515d
Roger-Rys <roger.reyesm97@gmail.com>
2022-05-05 21:28:26 -0500

    Modificacion en gifs-components

--- src/app/gifs/gifs-page/gifs-page.component.ts
+++ src/app/gifs/gifs-page/gifs-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { GifsService } from '../services/gifs.service';
 
 @Component({
   selector: 'app-gifs-page',
@@ -6,9 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GifsPageComponent implements OnInit {
 
-  constructor() { }
+  constructor(private gitsServices: GifsService) { }
 
   ngOnInit(): void {
+    if(this.gitsServices.historial.length==0){
+      this.gitsServices.buscarGrid("Bienvenido");
+    }
   }
 
 }
